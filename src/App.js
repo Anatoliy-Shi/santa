907c8b321afaa77843c7d05c80425666f3cffa4d
unknown <asmatol@yandex.ru>
2022-12-19 19:44:16 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import santa from './assets/santa.jpg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [donate, setDonate] = useState(['Владимир', 'Ирина', 'Кристина', 'Фёдор', 'Любовь',
+        'Вова', 'Александр', 'Алексей', 'Анатолий', 'Ринат'])
+    const [randomDonate, setRandomDonate] = useState(Math.floor(Math.random() * donate.length))
+
+    const [present, setPresent] = useState(['Владимир', 'Ирина', 'Кристина', 'Фёдор', 'Любовь',
+        'Вова', 'Александр', 'Алексей', 'Анатолий', 'Ринат'])
+    const [randomPresent, setRandomPresent] = useState(Math.floor(Math.random() * present.length))
+
+    useEffect(() => {
+        if (present[randomPresent] === donate[randomDonate]) {
+            setRandomDonate(Math.floor(Math.random() * donate.length))
+        }
+        if (donate[randomDonate] === undefined || present[randomPresent] === undefined) {
+            setRandomPresent(Math.floor(Math.random() * present.length))
+            setRandomDonate(Math.floor(Math.random() * donate.length))
+        }
+    }, [present, randomPresent, donate, randomDonate])
+
+
+    const handleClick = () => {
+        setRandomDonate(Math.floor(Math.random() * donate.length))
+        setRandomPresent(Math.floor(Math.random() * present.length))
+        setDonate(donate.filter((el, key) => key !== randomDonate))
+        setPresent(present.filter((el, key) => key !== randomPresent))
+
+    }
+    if (donate.length === 0) return <div className="App">
+        <div className="Container end">закончили</div>
     </div>
-  );
+
+    return (
+        <>
+            <div className="App">
+                <div className="Container">
+                    <img className="image" src={santa} alt="santa"/>
+                    <p>подарок дарит <br/> {donate[randomDonate]}</p>
+                    <p>подарок получает <br/> {present[randomPresent]}</p>
+                    <button onClick={handleClick}>
+                        следующий
+                    </button>
+                </div>
+            </div>
+        </>
+
+    );
 }
 
 export default App;
